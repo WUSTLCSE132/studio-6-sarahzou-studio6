6014ad1b0dfa6a8d4feb65ec43883fbbda8f7bd2
yumeng.zou <yumeng.zou@wustl.edu>
2017-10-09 15:55:14 -0500

    studio 6

---
+++ debounce/debounce.ino
@@ -0,0 +1,46 @@
+const int buttonPin = 2;
+
+int buttonState;
+int lastButtonState = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 200;
+
+int count = 0;
+
+void buttonPressed() {
+  //Serial.println("Interrupt");
+  count += 1;
+  Serial.println(count);
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Interrupts can happen on "edges" of signals.  
+  // Three edge types are supported: CHANGE, RISING, and FALLING 
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, CHANGE);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+
+  //detect change :-- start Timing
+  if(reading != lastButtonState){
+    lastDebounceTime = millis();
+  }
+
+  //time off :-- if truly changed, change buttonState
+  if((millis() - lastDebounceTime) > debounceDelay){
+    if(reading != buttonState){
+      buttonState = reading;
+      buttonPressed();
+    }
+  }
+
+  //update lastButtonState every loop
+  lastButtonState = reading;
+  
+//  Serial.println(count);
+}
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -7,30 +7,71 @@ public class SerialComm {
 	SerialPort port;
 
 	private boolean debug;  // Indicator of "debugging mode"
-	
+
 	// This function can be called to enable or disable "debugging mode"
 	void setDebug(boolean mode) {
 		debug = mode;
 	}	
-	
+
 
 	// Constructor for the SerialComm class
 	public SerialComm(String name) throws SerialPortException {
 		port = new SerialPort(name);		
 		port.openPort();
 		port.setParams(SerialPort.BAUDRATE_9600,
-			SerialPort.DATABITS_8,
-			SerialPort.STOPBITS_1,
-			SerialPort.PARITY_NONE);
-		
+				SerialPort.DATABITS_8,
+				SerialPort.STOPBITS_1,
+				SerialPort.PARITY_NONE);
+
 		debug = false; // Default is to NOT be in debug mode
 	}
-		
+
 	// TODO: Add writeByte() method from Studio 5
-	
+	public void writeByte(byte data){
+		try {
+			port.writeByte(data);
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("exception!");
+		}
+		if (debug){
+			System.out.println("<0x" + data + ">");
+		}
+
+
+	}
+
 	// TODO: Add available() method
-	
+	public boolean available() throws SerialPortException{
+		if (port.getInputBufferBytesCount() > 0){
+			return true;
+		}
+		return false;
+	}
+
 	// TODO: Add readByte() method	
-	
+	public byte readByte() throws SerialPortException{
+		return port.readBytes(1)[0];
+	}
+
 	// TODO: Add a main() method
+	public static void main(String[] args){
+		while(true){
+			try{
+				SerialComm sc = new SerialComm("COM3");
+				if (sc.available()){
+					char c = (char) sc.readByte();
+					System.out.print(c);
+				}
+			}
+			catch(SerialPortException e){
+				
+			}
+		}
+		
+
+
+
+	}
 }
